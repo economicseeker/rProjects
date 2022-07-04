824311c0a9b67ca4cc030fb621710ff83203c3b0
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2022-07-04 20:37:02 +0300

    Update README.r

--- README.r
+++ README.r
@@ -1,1 +1,16 @@
-# You can find datasets and r script in branches.
+########################################################## 
+# Project: 1.11.2.1 Chapter Exercises: Coder Edition
+# Purpose: Examine marijuana use in the United States 
+# Author: Azad
+# Edit date: July 2, 2022
+# Data: National Health and Nutrition Examination Survey (NHANES) 
+# Data: Marijuana Use in US
+# DUQ200 SAS LABEL Ever used marijuana or hashish 1 Y 2 N
+# RIDAGEYR: Age in years, at the time of the screening interview, is reported for
+# survey participants between the ages of 1 and 79 years of age. All responses of 
+# participants aged 80 years and older are coded as ‘80.’ The reporting of age in 
+# single years for adults 80 years and older was determined to be a disclosure 
+# risk. In NHANES 2015-2016, the weighted mean age for participants 80 years and 
+# older is 85 years.
+# RIAGNDR SAS Label Both males and females 0 YEARS - 150 YEARS 1 M 2 F
+##########################################################
