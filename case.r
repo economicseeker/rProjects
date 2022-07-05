bb9a00237a9ae66805a20d60b1d8c7e6213dca21
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2022-07-05 15:28:34 +0300

    Create case.r

---
+++ case.r
@@ -0,0 +1,17 @@
+#####################################################################################################################################################
+# CASE
+Use the National Health and Nutrition Examination Survey (NHANES) data to examine marijuana use in the United States. 
+# Spend a few minutes looking through the NHANES website (https://www.cdc.gov/nchs/nhanes/index.htm) before you begin, 
+# including finding the online codebook for the 2013–2014 data. Complete the following tasks to explore whether age is 
+# related to marijuana use in the United States.
+# 1) Open the 2013–2014 NHANES data file saved as nhanes_2013_ch1.csv with the book materials at edge.sagepub.com/harris1e (Achievement 4).
+# 2) Examine the data types for DUQ200, RIDAGEYR, and RIAGENDR, and fix data types if needed based on the NHANES codebook (Achievement 3).
+# 3) Based on the online NHANES codebook, code missing values appropriately for DUQ200, RIDAGEYR, and RIAGENDR (Achievement 5).
+# 4) Create a bar chart showing the percentage of NHANES participants answering yes and no to marijuana use (Achievement 6).
+# 5) Recode age into a new variable called age.cat with 4 categories: 18–29, 30–39, 40–49, 50–59 (Achievement 5).
+# 6) Create a bar chart of marijuana use by age group (Achievement 6).
+# 7) Add a prolog and comments to your code (Achievement 2).
+# 8) Following the R code in your code file, use comments to describe what you found. Given what you found and the information in the chapter,
+# what do you predict will happen with marijuana legalization in the next 10 years? Discuss how the omission of older people from the marijuana 
+# use question for NHANES influenced your prediction. Write your prediction and discussion in comments at the end of your code file (Achievement 2).
+#####################################################################################################################################################
